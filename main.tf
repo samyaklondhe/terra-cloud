228bf9e594b22d041a5ab9ceab2afa48187e9446
samyaklondhe <samyaklondhe0358@gmail.com>
2025-05-26 22:30:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,12 @@ provider "aws" {
 access_key = var.access_key
 secret_key = var.secret_key
 }
+variable = "access_key" {
+   type = string
+}
+variable = "secret_key" {
+   type = string
+}
 resource "aws_instance" "myinstance" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
