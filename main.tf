5043f7a67fc61ac631a188d5a94abc0dd0055d64
samyaklondhe <samyaklondhe0358@gmail.com>
2025-05-26 22:12:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,10 @@ terraform {
 }
 
 # Configure the AWS Provider
-
 provider "aws" {
   region     = "us-east-1"
+access_key = var.access_key
+secret_key = var.secret_key
 }
 resource "aws_instance" "myinstance" {
   ami           = "ami-084568db4383264d4"
