a154dac011e0c5399771e4c0fdda403b6ec9fd38
samyaklondhe <samyaklondhe0358@gmail.com>
2025-05-26 22:42:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,24 +8,26 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-access_key = var.access_key
-secret_key = var.secret_key
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
-variable = "access_key" {
-   type = string
+
+variable "access_key" {
+  type = string
 }
-variable = "secret_key" {
-   type = string
+
+variable "secret_key" {
+  type = string
 }
+
 resource "aws_instance" "myinstance" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
-  count = 2
-  tags = {
-   Name = "myinstance $(count.index+1"
-}
-}
+  count         = 2
 
+  tags = {
+    Name = "myinstance-${count.index + 1}"
+  }
+}
